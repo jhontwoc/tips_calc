8cb98859df2bbc39f762040c7704129f16de7ec9
Jhon Freyre <jhonalexfrey@hotmail.com>
2024-02-02 17:51:38 -0500

    add frontend base with vue-vite, ts and tailwind

---
+++ my-calc-tips-app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-3xl font-bold underline"> Hello </h1>
+  <HelloWorld /> 
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/navBar.vue'
+
+
+</script>
+
+<style scoped>
+</style>
